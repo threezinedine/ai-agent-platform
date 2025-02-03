a9a1849bfd0a840a8e8de7e67be0b4761cdda7c5
threezinedine <threezinedine@gmail.com>
2025-02-03 08:43:18 +0700

    remove failed test case.

--- server/utils/__tests__/test_password_hash.py
+++ server/utils/__tests__/test_password_hash.py
@@ -4,6 +4,3 @@ import unittest
 class TestPasswordHash(unittest.TestCase):
     def test_hash_password(self) -> None:
         self.assertTrue(True)
-
-    def test_fail_example(self) -> None:
-        self.assertTrue(False)
