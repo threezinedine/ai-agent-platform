2eff45bf78a9363114eb2de0c0cd0d96c8e5b86e
threezinedine <threezinedine@gmail.com>
2025-02-05 23:34:43 +0700

    add a new before test session code

--- server/conftest.py
+++ server/conftest.py
@@ -2,6 +2,9 @@ import os
 import sys
 from dotenv import load_dotenv
 
-load_dotenv()
+
+def pytest_sessionstart(session):
+    load_dotenv()
+
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ""))
