11e054301a601d4475908d50957f3500ff304d77
threezinedine <threezinedine@gmail.com>
2025-02-06 20:44:13 +0700

    clean the test-avatar in avatar folder for each test

--- server/conftest.py
+++ server/conftest.py
@@ -7,4 +7,13 @@ def pytest_sessionstart(session):
     pass
 
 
+def pytest_sessionfinish(session, exitstatus):
+    # remove all the files in the avatar folder except the default.png
+    load_dotenv()
+    avatar_folder = os.environ["AVATAR_FOLDER_DIR"]
+    for file in os.listdir(avatar_folder):
+        if file != "default.png":
+            os.remove(os.path.join(avatar_folder, file))
+
+
 sys.path.append(os.path.join(os.path.dirname(__file__), ""))
