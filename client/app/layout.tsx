65c7b9a9fd8de54531f5e45116a09d2bc94fd9d8
threezinedine <threezinedine@gmail.com>
2025-03-09 23:39:59 +0700

    [configuration] add metadata to the project

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/assets/globals.css';
 import Toast from './features/toast';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'AI Agent Platform',
+	description: 'An open-source project for building AI agents',
+	icons: {
+		icon: '/favicon.ico', // Place this file in the public directory
+		shortcut: '/b-logo.svg',
+	},
+};
 
 export default function RootLayout({
 	children,
